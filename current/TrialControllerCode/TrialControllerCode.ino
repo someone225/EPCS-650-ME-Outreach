cd960d102493faa3b5176678d14d09ec02081430
Gruffer <cestkowskij@gmail.com>
2025-11-05 14:40:11 -0500

    TrialControllerCode1.3

--- current/TrialControllerCode/TrialControllerCode.ino
+++ current/TrialControllerCode/TrialControllerCode.ino
@@ -3,18 +3,15 @@ Servo myservo; //create servo object to control a servo
 
 const int pos = 0; //variable to store the servo position
 
-const int ch1Rpin = 11; //this connects to channel 1 on rc receiver
+const int ch1Rpin = 6; //this connects to channel 1 on rc receiver
 const int ch3Rpin = 5; //this connects to channel 3 on rc receiver
 const int ENA = 9;
-const int IN1 = 10;
-const int IN2 = 8;
-
-const int ch3PpS = 1.929411765; //ch3 Pulse per speed aka 492/255 (difference in resting pulse to max pulse/max speed)
+const int IN1 = 12;
+const int IN2 = 7;
 const int deg = 90;
 
 void setup() 
 {
-
   //controler pins
   pinMode(ch1Rpin, INPUT);
   pinMode(ch3Rpin, INPUT);
@@ -23,8 +20,8 @@ void setup()
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
   //attaches the servo on pin 7
-  myservo.attach(7);
   Serial.begin(9600);
+  myservo.attach(4);
 }
 
 void loop() 
@@ -32,33 +29,45 @@ void loop()
   unsigned long ch1Pulse; //this is the length of the pulse that controller sends from channel 1
   unsigned long ch3Pulse; //this is the lenth of the pulse that controller sends from channel 3
   int ch1PpD; //ch1 Pulse per degree
+  int ch3PpS; //ch3 Pulse per speed
 
   //Forward and Reverse Movement
   ch3Pulse = pulseIn(ch3Rpin, HIGH); //records new pulse width for channel 3
-  if (ch3Pulse <= 1499)
+  if (ch3Pulse > 1455)
   { //checks if left joystick is moving forward
+    ch3PpS = 518/255;
     digitalWrite(IN1, HIGH);
     digitalWrite(IN2, LOW);
-    analogWrite(ENA, (1499-ch3Pulse)/ch3PpS); // Speed = 0 to 255 forward
-  } else if (ch3Pulse > 1499)
+    analogWrite(ENA, (ch3Pulse-1455)/ch3PpS); // Speed = 1 to 255 forward
+  } 
+  else if (ch3Pulse < 1240)
   { //checks if left joystick is moving in reverse
+    ch3PpS = 246/190;
     digitalWrite(IN1, LOW);
     digitalWrite(IN2, HIGH);
-    analogWrite(ENA, (ch3Pulse-1499)/ch3PpS); //Speed = 0 to 255 in reverse
+    analogWrite(ENA, (1240-ch3Pulse)/ch3PpS); //Speed = 1 to 190 in reverse
+  }
+  else if (ch3Pulse >= 1240 && ch3Pulse <= 1455)
+  { //checks if left joystick is inbetween third and fourth dash and if so sets speed to zero
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    analogWrite(ENA, 0); //Speed = 0
   }
 
   //Turning Movement
   ch1Pulse = pulseIn(ch1Rpin, HIGH); //records new pulse width for channel 1
-  if(ch1Pulse = 1520)
+  if(ch1Pulse >= 1370 && ch1Pulse <= 1385)
   { //checks if joystick is still
-    myservo.write(90); //sets wheels straight
-  } else if(ch1Pulse<1520)
-  { //checks if right joystick is moving left
-    ch1PpD = 496/deg;
-    myservo.write(((ch1Pulse-1520)/ch1PpD)+deg); //tells the servo how many degrees to turn to the left
-  } else if(ch1Pulse>1520)
+    myservo.write(deg); //sets wheels straight
+  } 
+  else if(ch1Pulse < 1370)
+  { //checks if right joystick is moving left  
+    ch1PpD = 376/deg;
+    myservo.write(((ch1Pulse-1370)/ch1PpD)+deg); //tells the servo how many degrees to turn to the left
+  } 
+  else if(ch1Pulse > 1385)
   { //checks if right joystick is moving right
-    ch1PpD = 470/deg;
-    myservo.write(((ch1Pulse-1520)/ch1PpD)+deg); //tells the servo how many degrees to turn to the right
+    ch1PpD = 502/deg;
+    myservo.write(((ch1Pulse-1385)/ch1PpD)+deg); //tells the servo how many degrees to turn to the right
   }
 }
