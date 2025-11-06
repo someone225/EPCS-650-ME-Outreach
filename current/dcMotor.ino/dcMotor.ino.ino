fda508615cea2c01c4ef5f610cec4529e799d8c1
someone225 <marksheng2020@gmail.com>
2025-11-06 09:05:43 -0500

    Update dcMotor.ino.ino

--- current/dcMotor.ino/dcMotor.ino.ino
+++ current/dcMotor.ino/dcMotor.ino.ino
@@ -1,3 +1,6 @@
+
+#include <multiMotor.h>
+
 #define enA 9
 #define enB 0
 #define in1 12
@@ -5,9 +8,7 @@
 #define in3 0
 #define in4 0
 
-#include <multiMotor.h>
-
-multiMotor testMotor
+multiMotor testMotor(in1, in2, in3, in4, enA, enB);
 void setup() {
   // put your setup code here, to run once:
   testMotor.begin();
@@ -16,7 +17,6 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   //digitalWrite(enA, HIGH);
-
-  testMotor.initDual
+  testMotor.initSingle(0, 0, 1, 0);
   
 }
